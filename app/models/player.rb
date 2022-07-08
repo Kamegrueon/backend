ff2f0dcf9983c990c3ef0ad77f52a1a2580dc97d
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2022-07-08 16:48:54 +0900

    feat: player data join cod

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ApplicationRecord
-  has_many :game_player_relations, dependent: :destroy, foreign_key: 'game_id'
+  has_many :game_player_relations, dependent: :destroy, foreign_key: 'player_id'
   has_many :games, through: :game_player_relations
+  has_many :cause_of_deaths
 
   validates :player_name, presence: true
 end
