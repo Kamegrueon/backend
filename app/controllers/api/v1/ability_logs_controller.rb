75d43993d1c0577c627421ebde7649e02e100d5d
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2022-08-12 10:58:51 +0900

    fix: params target_player_id

--- app/controllers/api/v1/ability_logs_controller.rb
+++ app/controllers/api/v1/ability_logs_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AbilityLogsController < ApplicationController
   def create
-    ability_log = AbilityLog.new(ability_log_params)    
+    ability_log = AbilityLog.new(ability_log_params)        
     if ability_log.save
       render json: ability_log
     else
