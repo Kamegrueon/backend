c24b0f8261ce106beb4d90c6c4ca72b8e720fdad
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2022-08-29 18:08:17 +0900

    fix: cod controller index action

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
         resources :game_rolls, only: :index
       end
       resources :dailies, defaults: {format: 'json'} do
-        resources :cause_of_deaths, only: [:index, :create]
+        resources :cause_of_deaths, only: [:create]
         patch 'cause_of_deaths', to: 'cause_of_deaths#update'
       end
+      get 'cause_of_deaths', to: 'cause_of_deaths#index'
       resources :votes, only: [:index, :create, :destroy]
       resources :coming_outs, only: [:index, :create, :edit] do 
         resources :ability_logs, only: [:create, :update, :destroy]
