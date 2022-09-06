7d189b4fbbc1e598413e74586190a052f065b6b1
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2022-09-06 10:41:06 +0900

    feat: devise controllers

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   private
 
     def sign_up_params
-      params.permit(:email, :password, :password_confirmation, :name)
+      params.require(:registration).permit(:email, :password, :password_confirmation, :name)
     end
 end
